f24c7c2513410a24a537c73e14180a35c7d1019d
62442katieb <kbott006@fiu.edu>
2019-06-10 11:14:25 +0200

    added github repo scraping#

--- bloom.js
+++ bloom.js
@@ -5,12 +5,15 @@ export default {
   data() {
     return {
       works: [],
+      repos: [],
       loading: true,
+      loading2: true,
       orcid: '0000-0002-7796-8795',
     };
   },
   mounted() {
     this.getOrcid('0000-0002-7796-8795');
+    this.getRepos('62442katieb');
   },
   methods: {
     getOrcid(orcid) {
@@ -22,11 +25,27 @@ export default {
       .then((resp) => resp.json())
       .then(data => {
         //console.log(data);
-        console.log(data.group[0]['work-summary'])
+        //console.log(data.group[0]['work-summary'])
         this.works = data.group;
         this.loading = false;
         //console.log(JSON.stringify(this.works));
         return this.works;
+      });
+    },
+    getRepos(ghusername) {
+      const options = {
+          method: 'GET',
+          headers: new Headers({'accept': 'application/json'})
+      };
+      fetch(`https://api.github.com/users/${ghusername}/repos`, options)
+      .then((resp) => resp.json())
+      .then(data => {
+        //console.log(data);
+        console.log(data[0])
+        this.repos = data;
+        this.loading2 = false;
+        console.log(JSON.stringify(this.repos));
+        return this.repos;
       });
     }
   },
